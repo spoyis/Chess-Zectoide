3c1e765c95fd19271678d4d1d914d7657a82d424
spoyis <leandrooliveira14051999@gmail.com>
2023-04-17 10:34:42 -0400

    fix: heuristic is no longer giga dumb

--- include/heuristics.h
+++ include/heuristics.h
@@ -6,12 +6,33 @@ namespace AI::Heuristic
 	template<bool evalForWhite>
 	double pieceScore(chess::GameState& game) 
 	{
-		const double pawnScore   = 1.0  * game.boardState.board[chess::pawn   + evalForWhite ? chess::white_pieces_offset : 0].populationCount();
-		const double bishopScore = 3.3  * game.boardState.board[chess::bishop + evalForWhite ? chess::white_pieces_offset : 0].populationCount();
-		const double knightScore = 3.0  * game.boardState.board[chess::knight + evalForWhite ? chess::white_pieces_offset : 0].populationCount();
-		const double kingScore   = 30.0 * game.boardState.board[chess::king   + evalForWhite ? chess::white_pieces_offset : 0].populationCount();
-		const double queenScore  = 9.0  * game.boardState.board[chess::queen  + evalForWhite ? chess::white_pieces_offset : 0].populationCount();
-		const double rookScore   = 5.0  * game.boardState.board[chess::rook   + evalForWhite ? chess::white_pieces_offset : 0].populationCount();
+		
+		const int allyOffset = evalForWhite ? chess::white_pieces_offset : 0;
+		const int enemyOffset = evalForWhite ? 0 : chess::white_pieces_offset;
+		//game.boardState.board[chess::pawn + allyOffset].debug("ALLY PAWNS");
+		//game.boardState.board[chess::pawn + enemyOffset].debug("ENEMY PAWNS");
+
+		const double allyPawnCount   = game.boardState.board[chess::pawn   + allyOffset].populationCount();
+		const double allyBishopCount = game.boardState.board[chess::bishop + allyOffset].populationCount();
+		const double allyKnightCount = game.boardState.board[chess::knight + allyOffset].populationCount();
+		const double allyKingCount   = game.boardState.board[chess::king   + allyOffset].populationCount();
+		const double allyQueenCount  = game.boardState.board[chess::queen  + allyOffset].populationCount();
+		const double allyRookCount   = game.boardState.board[chess::rook   + allyOffset].populationCount();
+
+		const double enemyPawnCount   = game.boardState.board[chess::pawn   + enemyOffset].populationCount();
+		const double enemyBishopCount = game.boardState.board[chess::bishop + enemyOffset].populationCount();
+		const double enemyKnightCount = game.boardState.board[chess::knight + enemyOffset].populationCount();
+		const double enemyKingCount   = game.boardState.board[chess::king   + enemyOffset].populationCount();
+		const double enemyQueenCount  = game.boardState.board[chess::queen  + enemyOffset].populationCount();
+		const double enemyRookCount   = game.boardState.board[chess::rook   + enemyOffset].populationCount();
+
+		const double pawnScore = (allyPawnCount - enemyPawnCount);
+		const double bishopScore = (allyBishopCount - enemyBishopCount) * 3.3;
+		const double knightScore = (allyKnightCount - enemyKnightCount) * 3.0;
+		const double kingScore = (allyKingCount - enemyKingCount) * 30.0;
+		const double queenScore = (allyQueenCount - enemyQueenCount) * 9.0;
+		const double rookScore = (allyRookCount - enemyRookCount) * 5.0;
+		std::cout << "EVALUATIION ------------------" << pawnScore + bishopScore + knightScore + kingScore + queenScore + rookScore << "----------\n";
 
 		return pawnScore + bishopScore + knightScore + kingScore + queenScore + rookScore;
 	}
